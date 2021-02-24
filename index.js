c5138beb5ee13f499034deee5d73e9f5d196bd5f
Renuka <renuka.mklb@gmail.com>
2021-02-24 21:17:33 +1100

    modified bootstrap

--- index.js
+++ index.js
@@ -14,18 +14,34 @@ const weatherCity = async (city) => {
 //   ).then((response) => response.json());
 
 const createCardHtml = (name, emoji, temp, feelsLike, description, img) =>
-  `<div class="card" d-flex  style="width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">${emoji}</h5>
+  //   `<div class="container ms-auto ">
+  //   <div class="card" d-flex justify-content-center >
+  //     <div class="card-body">
+  //       <h5 class="card-title">${emoji}</h5>
+  //       <h6 class="card-subtitle mb-2 text-muted">${name}</h6>
+  //       <p class="card-text"> ${temp}c,</br> feels like ${feelsLike},<br>${description}</p>
+
+  //     </div>
+  //   </div>
+  //   <div class='container pt-3 mx-3 image' style ='height:10px'>
+  //         <img src='${img}'>
+  //         </div>
+  //         </div>`;
+  `<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-2">
+    <div class="col-md-2">
+      <img src='${img}'>
+    </div>
+    <div class="col-md-10 text-center">
+    <h5 class="card-title">${emoji}</h5>
       <h6 class="card-subtitle mb-2 text-muted">${name}</h6>
       <p class="card-text"> ${temp}c,</br> feels like ${feelsLike},<br>${description}</p>
 
     </div>
   </div>
-  <div class='container pt-3 mx-3 image' style ='height:100px'>
-        <img src='${img}'>
-        </div>`;
-//   `<div class='card'>
+</div>`;
+
+//  style="width: 18rem;" `<div class='card'>
 //         <div class='row'>
 //             <div class='col-2'>
 //                 ${emoji}
@@ -83,28 +99,34 @@ searchButton.addEventListener("click", (event) => {
       const feelsLike = data.main.feels_like;
       const description = data.weather[0].description;
       let img;
-      if (description == "rain") {
-        img =
-          "https://images.unsplash.com/photo-1428592953211-077101b2021b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8cmFpbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&h=500&q=60";
-      } else if (description == "snow") {
-        img =
-          "https://images.unsplash.com/photo-1457269449834-928af64c684d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fHNub3d8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&h=500&q=60";
-      } else if (
-        description == "overcast clouds" ||
-        description == "broken clouds"
+      if (
+        description == "rain" ||
+        description == "light rain" ||
+        description == "shower"
       ) {
         img =
-          "https://images.unsplash.com/photo-1515890922410-ae767899d6b3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8Y2xvdWR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60";
+          "https://images.unsplash.com/photo-1428592953211-077101b2021b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8cmFpbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=w=200&h=200&q=60";
+      } else if (description == "snow" || description == "light snow") {
+        img =
+          "https://images.unsplash.com/photo-1457269449834-928af64c684d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fHNub3d8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&h=200&q=60";
+      } else if (
+        description == "overcast clouds" ||
+        description == "broken clouds" ||
+        description == "few clouds" ||
+        description == "scattered clouds"
+      ) {
+        img =
+          "https://images.unsplash.com/photo-1515890922410-ae767899d6b3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8Y2xvdWR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&h=200&q=60";
       } else if (description == "clear sky") {
         img =
-          "https://images.unsplash.com/photo-1540308990836-5a7b1df6dc00?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OTF8fGNsZWFyJTIwc2t5fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60";
+          "https://images.unsplash.com/photo-1540308990836-5a7b1df6dc00?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OTF8fGNsZWFyJTIwc2t5fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&h=200&q=60";
       } else if (
         description == "haze" ||
         description == "mist" ||
         description == "fog"
       ) {
         img =
-          "https://images.unsplash.com/photo-1533708985023-a9726305e9c8?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzV8fGhhemV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60";
+          "https://images.unsplash.com/photo-1533708985023-a9726305e9c8?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzV8fGhhemV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&h=200&q=60";
       }
 
       const cardHtml = createCardHtml(
